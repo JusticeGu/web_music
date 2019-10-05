03a2538ed9079e243bc70512fd669d41f64d9d0e
gxj <940331234@qq.com>
2019-10-05 15:40:16 +0800

    web music commit-3
    fix some bug
    update some function
    
    Signed-off-by: gxj <940331234@qq.com>

--- comment/urls.py
+++ comment/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
     path('',views.index),
     #path('<year>/<int:month>/<slug:day>',views.mydate)
-    re_path('(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2}).html',views.mydate),
+    #re_path('(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2}).html',views.mydate),
 
 
 ]
